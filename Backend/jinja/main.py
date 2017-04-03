b3285b3a47b914fb82014d7addc89c90e242734f
Jaspreet Mahal <jpjaspreetmahal@Jaspreets-MacBook-Pro.local>
2017-04-03 23:22:03 +0530

    Begin Blog Project

---
+++ Backend/jinja/main.py
@@ -0,0 +1,16 @@
+import webapp2
+import jinja2
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__),'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
